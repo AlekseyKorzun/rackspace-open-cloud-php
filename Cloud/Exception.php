9bbe9559f60ebf039d0bc7c7bd07d79b0bf0db59
Aleksey Korzun <al.ko@webfoundation.net>
2012-02-29 18:50:21 -0500

    Updating documentation, adding name spaces, better naming conventions

--- Cloud/Exception.php
+++
@@ -1,5 +0,0 @@
-<?php
-class Cloud_Exception extends Exception
-{
-
-}
---
+++ Cloud/Server/Exception.php
@@ -0,0 +1,16 @@
+<?php
+namespace Library\Cloud\Server;
+
+/**
+ * Cloud exception
+ *
+ * @package cloud
+ * @version 0.2
+ * @license bsd
+ * @author Aleksey Korzun <al.ko@webfoundation.net>
+ * @link http://github.com/AlekseyKorzun/php-cloudservers/
+ * @link http://www.alekseykorzun.com
+ */
+class Exception extends \Exception
+{
+}
