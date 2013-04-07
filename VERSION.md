1ef1122e8c0c0c47c51616e0bf43e92d5d4765c8
root <root@webfoundation.(none)>
2013-04-07 20:30:44 +0200

    Updating phpDocumentator 2 output and manual

---
+++ VERSION.md
@@ -0,0 +1,13 @@
+# Version History
+
+## April 4 2013
+
+Version 2.0 of the package has been released.
+
+### Changes
+
+* Support for 2.0 version of RackSpace cloud servers API
+
+## April 27 2010
+
+Initial revision was released
---
+++ src/Cloud/Server/Exception.php
@@ -0,0 +1,18 @@
+<?php
+namespace Cloud\Server;
+
+/**
+ * Cloud exception
+ *
+ * @package	Cloud
+ * @subpackage Cloud\Exception
+ * @version 0.3
+ * @license bsd
+ * @author Aleksey Korzun <al.ko@webfoundation.net>
+ * @link http://github.com/AlekseyKorzun/php-cloudservers/
+ * @link http://www.alekseykorzun.com
+ */
+class Exception extends \Exception
+{
+}
+
